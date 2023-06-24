e1871445f10dba85e9b645433ee8713c5a114934
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-06-24 15:01:38 -0400

    Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 export const metadata = {
   title: "Flexibbble",
   description: "Showcase your amazing projects",
@@ -11,9 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        Navbar
+        <Navbar />
         <main>{children}</main>
-        Footer
+        <Footer />
       </body>
     </html>
   );

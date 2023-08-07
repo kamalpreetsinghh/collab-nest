f02376927c4d962d77535a57e85d7dbd9deb2a31
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-08-07 10:14:25 -0400

    no message

--- lib/actions.ts
+++ lib/actions.ts
@@ -21,9 +21,7 @@ const apiUrl = isProduction
 const apiKey = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
   : "letmein";
-const serverUrl = isProduction
-  ? process.env.NEXT_PUBLIC_SERVER_URL
-  : "http://localhost:3000";
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL!;
 
 const client = new GraphQLClient(apiUrl);
 
